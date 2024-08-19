8f62a27724a70ba70d801adf1f518ec4fcac4d1b
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-08-19 13:47:55 +0530

    client initiated

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+        <Navbar />
+        <h1>Home Page</h1>
+        <Footer />
+    </div>
+  )
+}
