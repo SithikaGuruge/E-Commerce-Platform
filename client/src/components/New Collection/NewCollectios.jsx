9f7b8473df9135dddd3b52b7f8b75dc416d62c6f
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-08-22 09:21:53 +0530

    photos added to firebase and connect backend

--- client/src/components/New Collection/NewCollectios.jsx
+++ client/src/components/New Collection/NewCollectios.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import all_product from "../../assets/all_product.js";
 import Item from "../Item/item.jsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function NewCollections() {
+  const [all_product, setAllProduct] = useState([]);
+
+    useEffect(() => {
+      const fetchProducts = async () => {
+        try {
+          const response = await axios.get("http://localhost:4002/products");
+          console.log(response.data);
+          setAllProduct(response.data);
+        } catch (error) {
+          console.error("Error fetching products:", error);
+        }
+      };
+  
+      fetchProducts();
+    }, []);
   return (
     <div className=" flex flex-col lg:gap-10 gap-8 items-center mt-10">
       <div className="space-y-3">
--- client/src/components/Popular/popular.jsx
+++ client/src/components/Popular/popular.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import all_product from "../../assets/all_product.js";
+import { useEffect, useState } from "react";
 import Item from "../Item/item.jsx";
+import axios from "axios";
 
 export default function popular() {
+  const [all_product, setAllProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("http://localhost:4002/products");
+        console.log(response.data);
+        setAllProduct(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <div className=" flex flex-col lg:gap-10 gap-8 items-center ">
       <div className="space-y-3">
@@ -13,11 +29,12 @@ export default function popular() {
       </div>
       <div className="mx-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:gap-x-24 lg:gap-x-44 lg:gap-y-5 md:gap-y-5">
         {all_product.map((product, i) => {
-          if (product.category !== "women") return null;
+          if (product.category !== "women" || i>5 ) return null;
+          console.log("product", product);
           return (
             <Item
               key={i}
-              id={product.id}
+              id={product._id}
               name={product.name}
               image={product.image}
               new_price={product.new_price}
