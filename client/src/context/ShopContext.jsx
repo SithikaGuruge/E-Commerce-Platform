ae5bba41b169ab4688fbcb8a1242c130472c41c0
Sithika Guruge <sithikaguruge2001@gmail.com>
2024-08-22 09:21:53 +0530

    photos added to firebase and connect backend

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,9 +1,25 @@
 import React, { createContext } from 'react'
-import all_product from '../assets/all_product';
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) =>{
+    const [all_product, setAllProduct] = useState([]);
+
+    useEffect(() => {
+      const fetchProducts = async () => {
+        try {
+          const response = await axios.get("http://localhost:4002/products");
+          console.log(response.data);
+          setAllProduct(response.data);
+        } catch (error) {
+          console.error("Error fetching products:", error);
+        }
+      };
+  
+      fetchProducts();
+    }, []);
+    
     const contextValue = {all_product}
 
     return(
