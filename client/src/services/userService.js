b12ddcadb846ed1e4e1c0931c0b46c92f90b760f
Sithika Guruge <sithikaguruge2001@gmail.com>
2025-11-26 00:58:03 +0530

    fix: docker compose and nginx config files to deploy in aws

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_USER_API_URL || "http://localhost:4003";
+const API_URL = import.meta.env.API_URL || "http://localhost:4003";
 
 export const getUserById = async (userId) => {
   try {
